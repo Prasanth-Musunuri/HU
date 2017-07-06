5ecb997720030f2b4db5e5fddbe68f9acda1a1a8
Prasanth Musunuri <prasanth.musunuri@myob.com>
2017-07-07 09:28:27 +1000

    Adding more code

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/home/index' => 'home#index'
+  root 'home#index'
+  get '/home/about' => 'home#about'
+
+  post  '/questions' => 'home#temp'
+
+  get '/questions/:id' => 'home#question'
 end
